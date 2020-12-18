ba2de109731b285c398d9e32f5365ecfe116fe70
hulinhao <hulinhao@beadwallet.com>
2020-12-18 19:45:34 +0800

    20201208

--- store/api.js
+++ store/api.js
@@ -2,10 +2,10 @@
  * api  接口信息
  */
 const api = {
-    index : 'http://127.0.0.1:8081/api/index', //首页接口
-    catalog : 'http://127.0.0.1:8081/api/catalog', //查询目录
-    goods: 'http://127.0.0.1:8081/api/goods', //商品列表查询
-    getPage: 'http://127.0.0.1:8081/api/getPage' //分页测试接口
+    index : 'http://127.0.0.1:8888/api/index', //首页接口
+    catalog : 'http://127.0.0.1:8888/api/catalog', //查询目录
+    goods: 'http://127.0.0.1:8888/api/goods', //商品列表查询
+    getPage: 'http://127.0.0.1:8888/api/getPage' //分页测试接口
 };
 
 export default api;
